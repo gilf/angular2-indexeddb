8d7058867ab218d1a5c9bb58e6f95b1208f38a0d
Gil Fink <gil.fink@gmail.com>
2016-11-15 11:41:11 +0200

    Fix typings problem

---
+++ angular2-indexeddb.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="es6-promise" />
+export declare class AngularIndexedDB {
+    private utils;
+    private dbWrapper;
+    constructor(dbName: any, version: any);
+    createStore(version: any, upgradeCallback: any): Promise<any>;
+    getByKey(storeName: string, key: any): Promise<any>;
+    getAll(storeName: string): Promise<any>;
+    add(storeName: string, value: any, key: any): Promise<any>;
+    update(storeName: string, value: any, key: any): Promise<any>;
+    delete(storeName: string, key: any): Promise<any>;
+    openCursor(storeName: any, cursorCallback: (evt) => void): Promise<any>;
+    clear(storeName: string): Promise<any>;
+    getByIndex(storeName: string, indexName: string, key: any): Promise<any>;
+}
